909cab070fc1c5efd97a1d985c83809d5e894042
Zames.144p <sfranzcarlo@gmail.com>
2026-09-02 12:09:57 -0300

    colocando post e edit

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -19,4 +19,40 @@ class PostsController extends AppController {
         }
         $this->set('post', $post);
     }
+    public function add(){
+        if($this->request->is('post')){
+            $this->Post->create();
+            if($this->Post->save($this->request->data)){
+                $this->Flash->sucesse(__('Your post has been saved.'));
+                return $this->redirect(array('action' => 'index'));
+            }
+            $this->Flash->error(__('Unable to add your post.'));
+        }
+    }
+    public function edit($id = null){
+        //garante que esta tentando acessar um post
+        if(!$id){
+            throw new NotFoundException(__('Invalid post'));
+        }
+        //verifica se o post existe
+        $post = $this->Post->findById($id);
+        if(!$post){
+            throw new NotFoundException(__('Invalid post'));
+        }
+        //finalmente verifica se o post foi editado e salva as alterações
+        if($this->request->is(array('post', 'put'))){
+            $this->Post->id = $id;
+            if($this->Post->save($this->request->data)){
+                $this->Flash->sucesse(__('Your post has been updated.'));
+                return $this->redirect(array('action' => 'index'));
+            }
+            $this->Flash->error(__('Unable to update your post.'));
+        }
+        //posta depois da edição, para que o usuário possa ver o post editado
+        if($this->request->is('get')){
+            $this->request->data = $post;
+        }
+
+
+    }
 }
